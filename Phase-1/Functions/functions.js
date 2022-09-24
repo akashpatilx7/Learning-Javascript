3b1403903a308bc6bc671cca58e6ae4d04ee7a03
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2022-09-24 08:56:23 +0530

    Create functions.js

---
+++ Phase-1/Functions/functions.js
@@ -0,0 +1,22 @@
+// ****** Javascript Functions ******
+
+// NOTE : DRY PRINCIPLE ----> DO NOT REPEAT YOURSELF !
+
+function avg(a, b) {
+    c = (a + b) / 2;
+    return c;
+}
+
+c1 = avg(4, 6);
+c2 = avg(14, 16);
+
+console.log(c1, c2);
+
+// Functions come into picture only when we call them. Untill then, they remain unaffected in the code base.
+
+/* NOTE :
+
+BLUE Colour in Chrome Console ----> NUMBER 
+BLACK Colore in Chrome Console ----> STRING
+
+*/
