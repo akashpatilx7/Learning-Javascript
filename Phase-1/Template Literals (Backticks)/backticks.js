e6db0f7facfce83142346d82a45210cd1775c4f5
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2022-09-24 09:26:15 +0530

    Create backticks.js

---
+++ Phase-1/Template Literals (Backticks)/backticks.js
@@ -0,0 +1,6 @@
+// ****** Template Literals : Backticks ****** 
+
+a=34;
+console.log(`This is my ${a}`);
+
+// NOTE : From here onwards, we will not have to write 34 at all places where it has to be used. We will just write `${a}` to refer to 34.
