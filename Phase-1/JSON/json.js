cb21bf189872e1f014a810cb10f45b6d6a529b78
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2022-09-24 09:21:33 +0530

    Create json.js

---
+++ Phase-1/JSON/json.js
@@ -0,0 +1,23 @@
+// ****** JSON : Javascript Object Notation ****** 
+
+/*
+An open standard used to exchange data by converting it to string & can be converted into a Javascript Object. 
+
+typeof is used to find the type of a variable.
+*/ 
+
+
+// JSON.stringify 
+
+obj={name: "harry", length: 1, a: {this: "that"}};
+jso=JSON.stringify(obj);
+console.log(typeof jso);
+console.log(typeof obj);
+
+// NOTE : JSON standards use DOUBLE QUOTES only. They do not support Single Quotes.
+
+
+// JSON.parsed
+
+parsed=JSON.parse(`{"name": "harry", "length": 1, "a": {"this": "that"}}`);
+console.log(parsed);
