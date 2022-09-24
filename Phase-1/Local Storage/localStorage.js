7f16ed0f5862e0ab0b30d4bf9d73b7d3c78e0fc5
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2022-09-24 09:20:12 +0530

    Create localStorage.js

---
+++ Phase-1/Local Storage/localStorage.js
@@ -0,0 +1,21 @@
+// ****** : Local Storage In Javascript ******
+
+
+// Local Storage helps to save the data in users computers in the form of a "STRING".
+
+// Every domain has its Local Storage. 
+
+// Syntax to setItem 
+localStorage.setItem('name', 'Akash');
+
+// Syntax to show/ print  localStorage 
+localStorage;
+
+// Syntax to clear localStorage 
+localStorage.clear();
+
+// Syntax to get a particular previously setItem 
+localStorage.getItem();
+
+//Syntax to remove a particular item 
+localStorage.removeItem('name');
