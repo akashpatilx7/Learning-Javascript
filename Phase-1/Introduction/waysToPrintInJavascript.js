746b75940d7349920f7b29e7753cbf21e089de3f
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2022-09-24 08:44:01 +0530

    Create waysToPrintInJavascript.js

---
+++ Phase-1/Introduction/waysToPrintInJavascript.js
@@ -0,0 +1,7 @@
+/*
+****** 1. WAYS TO PRINT IN JAVASCRIPT ******
+*/
+
+console.log("Hello World");
+alert("Hello World");
+document.write("This function is used to write on the DOCUMENT directly without the use of console.log")
