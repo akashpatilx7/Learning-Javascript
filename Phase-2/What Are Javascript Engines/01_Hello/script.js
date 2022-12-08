97164f00d54d4b1f1ca694a0199d61425c4d5190
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2022-12-08 23:29:26 +0530

    Update script.js

--- Phase-2/What Are Javascript Engines/01_Hello/script.js
+++ Phase-2/What Are Javascript Engines/01_Hello/script.js
@@ -1,1 +1,27 @@
+console.log("Hello, Akash !");
 
+/*
+
+
+*** NOTE ***
+
+SHORTCUT TO OPEN TERMINAL (CTRL + ~)
+LISTING ALL THE FILES IN THE CURRENT FOLDER (dir)
+
+*/
+
+
+
+// If you are able to see the Message written in the console.log(" "), it means that we have succesfully injected/ able to load up our Javascipt into the HTML File.
+
+// A Javascript Engine is a Javascript Compiler that is present by-default in a Web Browser which compiler the written Javascript code and shows the output to the user in the Console.
+
+
+
+/*
+
+Node.js is an implementation of a Javascript Engine which can convert the stand-alone Javascript code into output-able code so that the user does not always have to attach the Javascript code into a HTML File just to reload/ process it.
+
+Once the user installs Node.js on the local system, a separate HTML File is no longer needed to run a Javascript File !
+
+*/
