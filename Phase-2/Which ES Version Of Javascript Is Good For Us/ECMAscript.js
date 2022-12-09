ae14ddf51e853afd584baeeb4952a99100618b61
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2022-12-09 19:05:11 +0530

    Add files via upload

---
+++ Phase-2/Which ES Version Of Javascript Is Good For Us/ECMAscript.js
@@ -0,0 +1,16 @@
+/*
+
+* HTML is the CONTENT, CSS makes the content BEAUTIFUL & Javascript adds DYNAMIC PROPERTIES to the content.
+
+* ECMA : European Computer Manufacturer Association 
+
+* ECMAscript is a Set of Guidelines to be followed by Scripting Languages like Javascript, Typescript in order to maintain uniformity throughtout the programmers while writing the code.
+  Javascript is a language that follows ECMAscript Guidelines.
+
+* The ES(N) version of Javascript comes up in 201(N-1)th year, generally.
+
+** The Versions of Javascript after ES6 are mostly Backward Compatible and not supportable in most of the advanced Web Browsers.
+
+*/
+
+console.log("ECMAScript")
