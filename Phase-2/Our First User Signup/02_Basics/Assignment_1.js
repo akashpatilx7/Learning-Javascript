69e913d971e8fe09f7de16b4dd7de5e94f429381
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2022-12-10 16:09:57 +0530

    Update Assignment_1.js

--- Phase-2/Our First User Signup/02_Basics/Assignment_1.js
+++ Phase-2/Our First User Signup/02_Basics/Assignment_1.js
@@ -18,3 +18,28 @@ USING THE INTERPOLATION (BACKTICKS) METHOD.
 
 
 */
+
+
+var firstName="Akash";
+var lastName="Patil";
+var email="akashpatilap876716@gmail.com";
+var password="1234567";
+var stateName="Maharashtra";
+var countryName="India";
+var courseCount=0;
+var isLoggedInFromGoogle=true;
+var isLoggedInFromFacebook=false;
+
+// ********* INTERPOLATION METHOD *********
+
+console.log(`
+             The firstName of the user is: ${firstName}.
+             The lastName of the user is: ${lastName}.
+             The email of the user is: ${email}.
+             The password of the user is: ${password}.
+             The user belongs to the state: ${stateName}
+             & the country: ${countryName}.
+             The user has purchased ${courseCount} Courses till now.
+             The user is logged in from Google is: ${isLoggedInFromGoogle}
+             while logged in from Facebook is: ${isLoggedInFromFacebook}.
+`);
