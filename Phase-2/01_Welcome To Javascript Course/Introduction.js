82adcaaf082c0b2754628b3ad475b2299fa19826
Akash <akashpatilap876716@gmail.com>
2023-02-23 00:18:28 +0530

    Phase-2

---
+++ Phase-2/01_Welcome To Javascript Course/Introduction.js
@@ -0,0 +1,14 @@
+/*
+
+Q. What is Javascript ? 
+A. It's just a Programming Language.
+
+# Java and Javascript are totally different languages.
+  Car and Carpet => Java and Javascript
+
+# Javascript is a standalone language i.e to learn Javascript, one does not require to learn any Prerequisite Language.
+
+*/
+
+
+console.log("Welcome to Javascript Course");
---
+++ Phase-2/10_Ternary Operator In Javascript/Ternary Operator.js
@@ -0,0 +1,29 @@
+/*
+
+The whole Operation present in the IF-ELSE CONDITION () should evaluate to a TRUE or a FALSE at the end.
+
+
+Q. Show an user a SIGNOUT Button if he is authenticated, otherwise show him an option to LOGIN/ SIGNUP.
+
+*/
+
+var authenticated = true;
+
+if(authenticated){
+  console.log("Show SIGNOUT Button");
+}
+
+else{
+  console.log("Show LOGIN/ SIGNUP Button");
+}
+
+
+
+/*
+
+TERNARY Operators : CONDITION ? if : else;
+
+authenticated ? "Show SIGNOUT Button" : "Show LOGIN/ SIGNUP Button";
+
+*/
+
