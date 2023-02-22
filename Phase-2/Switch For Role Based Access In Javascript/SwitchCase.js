a0427e3530105508d6d305d72a83f7c50a3478e5
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-02-22 10:46:29 +0530

    Add files via upload

---
+++ Phase-2/Switch For Role Based Access In Javascript/SwitchCase.js
@@ -0,0 +1,45 @@
+/*
+
+If we have MULTIPLE things to checkout & provide certain Specific Messages based on them, we use SWITCH CASE instead of IF_ELSE.
+
+
+switch (key (VALUE THAT NEEDS TO BE EVALUATED)){
+  case value:
+    break;
+  
+  default:
+    break;
+}
+
+
+Q. Create an application with the following roles : 
+   admin : gets full access
+   subadmin : gets access to create/ delete courses 
+   testprep : gets access to create/ delete tests
+   user : gets access to consume content 
+   
+*/
+
+var user = "testprep";
+
+switch(user){
+  case "admin":
+    console.log("gets full access");
+    break;
+  
+  case "subadmin":
+    console.log("gets access to create/ delete courses");
+    break;
+
+  case "testprep":
+    console.log("gets access to create/ delete tests");
+    break;
+
+  case "user":
+    console.log("gets access to consume content");
+    break;
+
+  default:
+    console.log("Trial User");
+    break;
+}
