2e23dd86b996e0a82ecc307e08934fb3a1344f1a
Akash <akashpatilap876716@gmail.com>
2023-02-27 19:51:51 +0530

    Phase-2

---
+++ Phase-2/18_Light Intro To This In Javascript/This.js
@@ -0,0 +1,5 @@
+// THIS keyword shows what the Global Context holds & what properties are inside it, currently.
+
+console.log(this); // This line of code will return an "Empty Scope ({})" in a NODE ENGINE whereas a Global COntext i.e. WINDOW in the BROWSER
+
+// NOTE : Global Context differs according to the situation.
